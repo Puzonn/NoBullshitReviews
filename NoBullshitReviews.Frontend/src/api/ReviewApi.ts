a209ab1fa4994bf5d19b0314780cab3f53092c7d
Puzonn <przemekpyszkiewicz@gmail.com>
2025-03-04 09:57:16 +0100

    Frontend: Use URI from .env and expose .env file from .gitignore

--- NoBullshitReviews.Frontend/src/api/ReviewApi.ts
+++ NoBullshitReviews.Frontend/src/api/ReviewApi.ts
@@ -1,5 +1,20 @@
+export const getBaseUri = () => {
+  let url;
+  switch (process.env.NODE_ENV) {
+    case "production":
+      url = process.env.REACT_APP_API_PRODUCTION_URI;
+      break;
+    case "development":
+      url = process.env.REACT_APP_API_DEBUG_URI;
+    default:
+      url = process.env.REACT_APP_API_DEBUG_URI;
+  }
+
+  return url;
+};
+
 export const PostReview = async (form: FormData) => {
-  return await fetch("https://localhost:7106/review/create", {
+  return await fetch(`https://${getBaseUri()}/review/create`, {
     credentials: "include",
     method: "POST",
     body: form,
@@ -7,32 +22,32 @@ export const PostReview = async (form: FormData) => {
 };
 
 export const FetchFeed = async () => {
-  return await fetch("https://localhost:7106/feed", {
+  return await fetch(`https://${getBaseUri()}/feed`, {
     method: "GET",
   });
 };
 
 export const FetchQuery = async (query: string) => {
-  return await fetch(`https://localhost:7106/review/query?${query}`, {
+  return await fetch(`https://${getBaseUri()}/review/query?${query}`, {
     credentials: "include",
     method: "GET",
   });
 };
 
 export const FetchGame = async (name: string) => {
-  return await fetch(`https://localhost:7106/game/${name}`, {
+  return await fetch(`https://${getBaseUri()}/game/${name}`, {
     method: "GET",
   });
 };
 
 export const FetchGamesFeed = async () => {
-  return await fetch(`https://localhost:7106/feed/games`, {
+  return await fetch(`https://${getBaseUri()}/feed/games`, {
     method: "GET",
   });
 };
 
 export const PostGame = async (form: FormData) => {
-  return await fetch(`https://localhost:7106/game/create-base`, {
+  return await fetch(`https://${getBaseUri()}/game/create-base`, {
     body: form,
     credentials: "include",
     method: "POST",
@@ -40,13 +55,13 @@ export const PostGame = async (form: FormData) => {
 };
 
 export const FetchMovie = async (name: string) => {
-  return await fetch(`https://localhost:7106/movie/${name}`, {
+  return await fetch(`https://${getBaseUri()}/movie/${name}`, {
     method: "GET",
   });
 };
 
 export const PostMovie = async (form: FormData) => {
-  return await fetch(`https://localhost:7106/movie/create-base`, {
+  return await fetch(`https://${getBaseUri()}/movie/create-base`, {
     body: form,
     credentials: "include",
     method: "POST",
@@ -54,7 +69,7 @@ export const PostMovie = async (form: FormData) => {
 };
 
 export const FetchLatestGames = async () => {
-  return await fetch(`https://localhost:7106/game/latest`, {
+  return await fetch(`https://${getBaseUri()}/game/latest`, {
     method: "GET",
   });
 };
